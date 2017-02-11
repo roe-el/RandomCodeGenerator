4967a5e8e749439b1609f71914c43e7ea4e943f8
roe-el <rogar28@gmail.com>
2017-02-11 03:50:05 -0600

    Add index, js, and style files

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    //Store quotes
+    var quoteBank = [];
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&callback=", function(data) {
+
+        data.forEach(function(val) {
+            quoteBank.push(val);
+        })
+        changeQuote();
+    });
+
+    function changeQuote() {
+
+        randomQuote = Math.floor((Math.random() * 29))
+            //Setting text from json object
+        $("#text").html(quoteBank[randomQuote].content);
+        $("#author").text(quoteBank[randomQuote].title);
+
+    }
+    //Call function on click
+    $("#getQuote").on('click', function() {
+        changeQuote();
+
+    });
+    //Twitter Quote Button-had to use .text() to remove tags
+    $(".tweetQuote").click(function() {
+
+        $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + $(quoteBank[randomQuote].content).text() + "  -" + quoteBank[randomQuote].title);
+    });
+});
